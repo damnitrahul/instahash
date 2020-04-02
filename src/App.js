44d383f912bbc2f626aa8838083c9af12fb3c5f8
Rahul Raj <damnitrahul@gmail.com>
2020-04-02 14:37:12 +0530

    Console Log Added

--- src/App.js
+++ src/App.js
@@ -7,10 +7,25 @@ import Downloader from './components/downloader/Downloader';
 import Footer from './components/footer/Footer';
 
 function App() {
-  // const [appState, setAppState] = useState('hash');
-  // const handleStateChange = e => {
-  //   setAppState(e.target.getAttribute('name'));
-  // };
+  console.log(
+    '%c 👋 Oh, Hi there,Curious web lover!',
+    'color: #d74034; font-size:24px;'
+  );
+  console.log(
+    `%c  ✨ Like what you see? We could work together!
+    
+  🐛 Found a bug? Please, let me know by e-mail, twitter, github issue...
+
+  🛠 Code available here: https://github.com/damnitrahul/instahash
+    
+  📬 Find me here: https://twitter.com/damnitrahul
+    
+  Let the debug begin!
+  
+  Bonus meme: https://preview.redd.it/xxeyh0e833711.gif?format=mp4&s=5fe8fde9cb56528721c04741f1ac7cfb14ef2053
+  `,
+    'font-size:16px'
+  );
   return (
     <div id="App">
       <Navbar />
